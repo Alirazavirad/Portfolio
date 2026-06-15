7316eea6e77b0060c4f5437ca780d68e45d6541c
Alirazavirad <ali.razavi.rad002@gmail.com>
2026-06-16 00:07:26 +0330

    all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden pt-[80px]">
       <section id="home">
         <Header />
       </section>
